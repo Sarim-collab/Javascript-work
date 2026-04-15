a9d1cab31f7bd4539ec227209d0f0bd6c1d2933d
Sarim-collab <sarimhanfi56@gmail.com>
2026-04-15 18:50:56 +0500

    assignment 2 pending

---
+++ Assignment 2/app.js
@@ -0,0 +1,21 @@
+// question 1
+let userNAME;
+
+// question 2
+let myName = "M.Sarim Hanfi";
+
+// question 3
+// part a
+let message;
+
+// part b
+message ="Hello World"
+
+// part c
+alert(message)
+
+// question 4
+let studentName ="Jhone Doe"
+let studentAge ="15 year old"
+let studentCourse ="Certified mobile application development"
+
