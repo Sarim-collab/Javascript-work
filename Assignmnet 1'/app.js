38d2ebe3c75188e06650275c97eea48c2719067b
Sarim-collab <sarimhanfi56@gmail.com>
2026-04-15 18:32:30 +0500

    assignment 1 complete

---
+++ Assignmnet 1'/app.js
@@ -0,0 +1,15 @@
+// question 1
+// alert("Hello thanks for visiting our website")
+
+// question 2
+// alert("Error!Please enter a valid password")
+
+// question 3
+// alert("Welcome to JS land..\nHappy coding")
+
+// question 4
+// alert("Welcome to JS land..")
+// alert("Happy coding!\nPrevent this page from creating additional dialogs")
+
+// question 5
+// alert("Hello... I can run JS through my web browser's console")
