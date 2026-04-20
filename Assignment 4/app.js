7764c00216c854d68fd2cd2aef0cf5a110a27eb8
Sarim-collab <sarimhanfi56@gmail.com>
2026-04-20 18:56:05 +0500

    jhsdka

---
+++ Assignment 4/app.js
@@ -0,0 +1,19 @@
+// // question 1
+// let studentName , studentId , studentAge
+
+// // question 2
+// // illegal
+// let student Name;
+// let 56studentAge;
+// let -studentname;
+// let let let
+
+// // legal
+// let studentName;
+// let studentAge56;
+// let _studentName;
+// let $studentName;
+// let student6325name;
+
+// question 3
+document.write("<h1>Rules for naming JS variables</h1>")
