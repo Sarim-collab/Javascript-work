4e29a6aa76dc5e2f217f13dca6f366555dfbc1d7
Sarim-collab <sarimhanfi56@gmail.com>
2026-04-20 18:40:40 +0500

    Assignment 3

---
+++ Assignment 3/app.js
@@ -0,0 +1,19 @@
+// // question 1
+// let age= 14
+// alert("I am" + age + " years old")
+
+// // question 2
+// let noOfVisits= 14
+// alert("You have visited this site "+ noOfVisits+ " times")
+
+// // question 3
+// let birthYear = 2012
+// document.write("My birth year is "+ birthYear)
+// document.write("<br/>")
+// document.write("Data type of my declared variable is number")
+
+// // question 4
+// let visitorName="Ahmed"
+// let quantity="5"
+// let productTitle="T-shirts"
+// document.write( visitorName +" ordered " + quantity + productTitle + "(s) on XYZ clothing store")
