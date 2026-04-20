83e16e943a0453a565b82a3f022d432fbcaa355e
Sarim-collab <sarimhanfi56@gmail.com>
2026-04-20 18:11:38 +0500

    Assignmnet 2 complete

--- Assignment 2/app.js
+++ Assignment 2/app.js
@@ -1,21 +1,45 @@
-// question 1
-let userNAME;
+// // question 1
+// let userNAME;
 
-// question 2
-let myName = "M.Sarim Hanfi";
+// // question 2
+// let myName = "M.Sarim Hanfi";
 
-// question 3
-// part a
-let message;
+// // question 3
+// // part a
+// let message;
 
-// part b
-message ="Hello World"
+// // part b
+// message ="Hello World"
 
-// part c
-alert(message)
+// // part c
+// alert(message)
 
-// question 4
-let studentName ="Jhone Doe"
-let studentAge ="15 year old"
-let studentCourse ="Certified mobile application development"
+// // question 4
+// let studentName ="Jhone Doe"
+// let studentAge ="15 year old"
+// let studentCourse ="Certified mobile application development"
 
+// alert(studentName);
+// alert(studentAge);
+// alert(studentCourse);
+
+// // question 5
+// let foodName ="PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(foodName)
+
+// // question 6
+// let email ="dummyemail@gmail.com"
+// alert(email)
+
+// // question 7
+// let book ="I am trying tom learn from the book a smarter\n way to learn javascript"
+// alert(book)
+
+
+// // question 8
+// let script ="Yeh i can write html content through javascript"
+// document.write(script)
+
+// // question 9
+// let design ="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(design)
