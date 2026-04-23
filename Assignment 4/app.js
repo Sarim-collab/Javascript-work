c2d80fbb429eacfe4bf57e125a999c76f747b3b8
Sarim-collab <sarimhanfi56@gmail.com>
2026-04-23 23:28:59 +0500

    Assignment 4 complete

--- Assignment 4/app.js
+++ Assignment 4/app.js
@@ -16,14 +16,17 @@
 // let student6325name;
 
 // question 3
-document.write("<h1>Rules for naming JS variables</h1>")
-let variableName1 = "numbers"
-let variableName2 = "$"
-let variableName3 = "_"
-let variableName4 = "letter"
-let variableName5 = "$"
-let variableName6 = "_"
-let variableName7 = "sensitive"
-let variableName8 = "keywords"
+// document.write("<h1>Rules for naming JS variables</h1>")
+// let variableName1 = "numbers"
+// let variableName2 = "$"
+// let variableName3 = "_"
+// let variableName4 = "letter"
+// let variableName5 = "$"
+// let variableName6 = "_"
+// let variableName7 = "sensitive"
+// let variableName8 = "keywords"
 
-document.write("Variable names can only contain " + variableName1 + ", " + variableName2 + ", " + variableName3 )
+// document.write("Variable names can only contain " + variableName1 + ", " + variableName2 + " ,and " + variableName3 + " . For example: $my_1st variable <br>")
+// document.write("Variables must begin with a " + variableName4 + ", " + variableName5 + " or " + variableName6 + ". for example: $name, _name or name <br>")
+// document.write("Variable names are case " + variableName7 + "<br>")
+// document.write("Variable names should be in JS " + variableName8 + "<br>")
